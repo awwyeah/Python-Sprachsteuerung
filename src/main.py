c80617c635df180a1f9d44ec725937ed58897e3f
Leon Barth <leon.barth2@gmail.com>
2017-03-29 20:32:35 +0200

    add main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import audiostream
+import decoding_audio
+import sys
+
+if sys.argv[1] == "record":
+    audiostream.record(sys.argv[2])
+elif sys.argv[1] == "output":
+    audiostream.audio_output(sys.argv[2])
+elif sys.argv[1] == "decoding":
+    decoding_audio.decoding(sys.argv[2])
+elif sys.argv[1] == "live_decoding":
+    decoding_audio.live_decoding()
+
